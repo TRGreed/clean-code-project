c19faaa54242492ee94258f5d29eee8251175ec0
GREED <thegreed2903@yandex.ru>
2026-05-29 23:17:17 +0300

    [Практическое занятие 14] 1) Добавлены тесты для сервиса

---
+++ src/test/java/ru/greed/cleancodeproject/OrderServiceTest.java
@@ -0,0 +1,47 @@
+package ru.greed.cleancodeproject;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.greed.cleancodeproject.model.Item;
+import ru.greed.cleancodeproject.service.OrderService;
+import java.util.Collections;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Расчет корзины")
+class OrderServiceTest {
+
+    private final OrderService orderService = new OrderService();
+
+    @Test
+    @DisplayName("Обычный тариф")
+    void regular() {
+        List<Item> cart = Collections.singletonList(new Item("Книга", 100.0, 2));
+        double total = orderService.calc(cart, "REGULAR");
+        assertEquals(200.0, total, 0.001);
+    }
+
+    @Test
+    @DisplayName("Скидка VIP")
+    void vip() {
+        List<Item> cart = Collections.singletonList(new Item("Книга", 100.0, 2));
+        double total = orderService.calc(cart, "VIP");
+        assertEquals(180.0, total, 0.001);
+    }
+
+    @Test
+    @DisplayName("Скидка NEW")
+    void newCustomer() {
+        List<Item> cart = Collections.singletonList(new Item("Книга", 100.0, 2));
+        double total = orderService.calc(cart, "NEW");
+        assertEquals(190.0, total, 0.001);
+    }
+
+    @Test
+    @DisplayName("Чек выше 1000")
+    void expensive() {
+        List<Item> cart = Collections.singletonList(new Item("Ноутбук", 1200.0, 1));
+        double total = orderService.calc(cart, "REGULAR");
+        assertEquals(1150.0, total, 0.001);
+    }
+}
