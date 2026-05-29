162755932619399cff21c95a656fecd5bf491e85
GREED <thegreed2903@yandex.ru>
2026-05-29 23:08:00 +0300

    Initial commit

---
+++ src/main/java/ru/greed/cleancodeproject/CleanCodeProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.greed.cleancodeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanCodeProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/greed/cleancodeproject/service/OrderService.java
@@ -0,0 +1,28 @@
+package ru.greed.cleancodeproject.service;
+
+import ru.greed.cleancodeproject.model.Item;
+
+import java.util.List;
+
+public class OrderService {
+    public double calc(List<Item> items, String type) {
+        double s = 0;
+        for (Item i : items) {
+            s += i.getPrice() * i.getQuantity();
+        }
+
+        if (type.equals("VIP")) {
+            s = s * 0.9;
+        }
+
+        if (type.equals("NEW")) {
+            s = s * 0.95;
+        }
+
+        if (s > 1000) {
+            s = s - 50;
+        }
+
+        return s;
+    }
+}
